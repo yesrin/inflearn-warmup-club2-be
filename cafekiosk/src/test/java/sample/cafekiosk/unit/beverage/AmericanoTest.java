0e3cbf40911b262c765f43fc8484a9a36a9af142
yesrin <yerin.seo@mataedu.com>
2024-10-13 17:32:02 +0900

    [추가-테스트] 키오스크 기본 로직

---
+++ cafekiosk/src/test/java/sample/cafekiosk/unit/beverage/AmericanoTest.java
@@ -0,0 +1,23 @@
+package sample.cafekiosk.unit.beverage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AmericanoTest {
+
+    @Test
+    void getName() {
+        Americano americano = new Americano();
+        //assertEquals("아메리카노", americano.getName());
+        assertThat(americano.getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void getPrice() {
+        Americano americano = new Americano();
+
+        assertThat(americano.getPrice()).isEqualTo(4000);
+    }
+}
